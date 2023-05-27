09bf25262d9b08b2d6b53b8a4b7ebb8debea7f31
김국진 <91815971+KukJinKim0211@users.noreply.github.com>
2023-05-28 08:15:34 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.35 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = [];
+    for (let i = 0; i < n; i++) {
+        const array = [];
+        for (let j = 0; j < n; j++) {
+            if (i == j) array.push(1)
+            else array.push(0)
+        }
+        answer.push(array)
+    }
+    return answer;
+}
