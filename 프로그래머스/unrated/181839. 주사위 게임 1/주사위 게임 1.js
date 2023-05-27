56a6d1d775871cc477fbf7a096e07017e06a8e40
김국진 <91815971+KukJinKim0211@users.noreply.github.com>
2023-05-27 19:05:41 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+    if (a % 2 && b % 2)
+        return a * a + b * b
+    else if (a % 2 || b % 2)
+        return 2 * (a + b)
+    else 
+        return a > b ? a - b : b - a;
+}
