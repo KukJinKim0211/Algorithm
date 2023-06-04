43df4db14ab36ba69e879ad4c36d60c6756e885e
김국진 <91815971+KukJinKim0211@users.noreply.github.com>
2023-06-04 16:56:32 +0900

    [unrated] Title: 배열 만들기 2, Time: 15.50 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,8 @@
+function solution(l, r) {
+    var answer = [];
+    for (let i = l; i <= r; i++) {
+        if (!String(i).replaceAll("5", "").replaceAll("0", ""))
+            answer.push(i)
+    }
+    return answer.length ? answer : [-1];
+}
